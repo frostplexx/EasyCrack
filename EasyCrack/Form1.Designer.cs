b21767e2abb96893240f8f852e902af4bd07886f
Daniel Inama <daniel.inama02@gmail.com>
2022-11-06 14:14:14 +0100

    small ui changes

--- EasyCrack/Form1.Designer.cs
+++ EasyCrack/Form1.Designer.cs
@@ -55,9 +55,12 @@ namespace EasyCrack
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.button4 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -75,7 +78,7 @@ namespace EasyCrack
             this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(12, 280);
             this.button1.Name = "button1";
@@ -90,11 +93,11 @@ namespace EasyCrack
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(71)))), ((int)(((byte)(94)))));
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(229, 280);
             this.button2.Margin = new System.Windows.Forms.Padding(0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(70, 28);
+            this.button2.Size = new System.Drawing.Size(72, 28);
             this.button2.TabIndex = 12;
             this.button2.Text = "Help";
             this.button2.UseVisualStyleBackColor = false;
@@ -104,11 +107,11 @@ namespace EasyCrack
             // 
             this.progressBar1.BackColor = System.Drawing.SystemColors.Window;
             this.progressBar1.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::EasyCrack.Properties.Settings.Default, "downprog", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.progressBar1.ForeColor = System.Drawing.Color.IndianRed;
+            this.progressBar1.ForeColor = System.Drawing.Color.Transparent;
             this.progressBar1.Location = new System.Drawing.Point(12, 315);
             this.progressBar1.Margin = new System.Windows.Forms.Padding(0);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(287, 23);
+            this.progressBar1.Size = new System.Drawing.Size(289, 23);
             this.progressBar1.TabIndex = 13;
             this.progressBar1.Value = global::EasyCrack.Properties.Settings.Default.downprog;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
@@ -170,6 +173,7 @@ namespace EasyCrack
             // comboBox2
             // 
             this.comboBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBox2.ForeColor = System.Drawing.Color.Black;
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(6, 80);
@@ -201,7 +205,7 @@ namespace EasyCrack
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(76, 21);
             this.button3.TabIndex = 24;
-            this.button3.Text = "Search";
+            this.button3.Text = "⌕ Search";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -243,7 +247,7 @@ namespace EasyCrack
             this.browse.Name = "browse";
             this.browse.Size = new System.Drawing.Size(76, 22);
             this.browse.TabIndex = 19;
-            this.browse.Text = "Browse Files";
+            this.browse.Text = " 🗁 Browse";
             this.browse.UseVisualStyleBackColor = false;
             this.browse.Click += new System.EventHandler(this.browse_Click_1);
             // 
@@ -293,7 +297,7 @@ namespace EasyCrack
             // 
             this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
             this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBox1.ForeColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.InfoText;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "arabic",
@@ -382,6 +386,7 @@ namespace EasyCrack
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(26)))), ((int)(((byte)(33)))));
+            this.panel1.Controls.Add(this.label8);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
@@ -391,6 +396,17 @@ namespace EasyCrack
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownEvent);
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMoveEvent);
             this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 5);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 21);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "EasyCrack";
             // 
             // EasyCrack
             // 
@@ -421,6 +437,8 @@ namespace EasyCrack
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -452,6 +470,8 @@ namespace EasyCrack
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.HelpProvider helpProvider1;
     }
 }
 
